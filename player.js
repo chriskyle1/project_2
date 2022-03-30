e1a0f66ef4906b82a6b864c1021e14b85e245afd
chriskyle1 <ckylestem2020@gmail.com>
2022-03-30 16:34:16 -0400

    updates to player

--- player.js
+++ player.js
@@ -1,13 +1,33 @@
-const fName = document.querySelector('input')
+const input = document.querySelector('input')
 const button = document.querySelector('button')
-const infoDiv = document.querySelector('div')
-
+const infoDiv = document.getElementById('name')
+const heightDiv = document.getElementById('height')
+const weightDiv = document.getElementById('weight')
+const postDiv = document.getElementById('post')
 
 button.addEventListener('click', async () => {
-    let id = fName.value
-    let response = await axios.get(`https://www.balldontlie.io/api/v1/players/?search=${id}`)
-    console.log(response.data)
+    let idName = input.value
+    let response = await axios.get(`https://www.balldontlie.io/api/v1/players/?search=${idName}`) 
+    let playerId = response.data.data[0].id
+    console.log(playerId)
 
-    let playerName = response.data[2]
-    infoDiv.innerHTML = `${playerName}`
+    let fName = response.data.data[0].first_name
+    let lName = response.data.data[0].last_name
+    let heightI = response.data.data[0].height_inches
+    let heightF = response.data.data[0].height_feet
+    let weight = response.data.data[0].weight_pounds
+    let position = response.data.data[0].position
+    let team = response.data.data[0].team.full_name
+    console.log(response)
+
+    let averages = await axios.get(`https://www.balldontlie.io/api/v1/stats?player_ids[]=${playerId}`)
+    console.log(averages)
+
+    let threePointAvg = averages.data.data[0].fg3_pct
+    
+
+    infoDiv.innerHTML = `<h1>${fName} ` + `${lName}</h1>`
+    heightDiv.innerHTML = `Height: ${heightF} Feet ` + `${heightI} Inches`
+    weightDiv.innerHTML = `Weight: ${weight} pounds`
+    postDiv.innerHTML = `Position: ${position}<br>Team: ${team}`
 })
--- team.js
+++ team.js
@@ -4,7 +4,8 @@ const teamName = document.querySelector('input')
 
 button.addEventListener('click', async () => {
     let id = teamName.value
-    let response = await axios.get(`https://www.balldontlie.io/api/v1/teams/?=${id}`)
+    console.log(id)
+    let response = await axios.get(`https://www.balldontlie.io/api/v1/teams/${id}`)
     console.log(response)
 
 
